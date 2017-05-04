f85f9bf811a919545338411a95adb7bfd053343f
PBarna <barnabas.peti@gmail.com>
2017-05-04 13:29:31 +0200

    Revert "Cart and order"

--- src/main/java/com/codecool/shop/dao/implementation/SupplierDaoMem.java
+++ src/main/java/com/codecool/shop/dao/implementation/SupplierDaoMem.java
@@ -35,7 +35,8 @@ public class SupplierDaoMem implements SupplierDao {
     }
 
     @Override
-    public void remove(int id) { DATA.remove(find(id));
+    public void remove(int id) {
+        DATA.remove(find(id));
     }
 
     @Override
